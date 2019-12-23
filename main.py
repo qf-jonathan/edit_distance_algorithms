e3644563ddd9a587daad6dec951d4d10a9813d9c
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2019-12-23 08:10:15 -0500

    some little changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     tree_b = IBKTree()
     tree_c = BruteForceDistance()
     tree_d = TrieDist()
-    with open('wold_list_spanish.txt', encoding='utf8') as spanish:
+    with open('spanish.txt', encoding='utf8') as spanish:
         list_words = [wd.rstrip('\n').lower() for wd in spanish.readlines()]
 
         def insert_test(tree, name):
@@ -18,7 +18,9 @@ def main():
             cnt = 1
             for wd in list_words:
                 tree.add(wd)
-                print('\r[{}] [{:.2f}%]'.format(name, cnt / len(list_words) * 100), end='')
+                print('\r[{}] [{:.2f}%]'.format(
+                    name, cnt / len(list_words) * 100), end=''
+                )
                 cnt += 1
             print(' [{:.8f}s]'.format(time.time() - start_time))
 
@@ -35,7 +37,8 @@ def main():
             start_time = time.time()
             similar = tree.get_similar(word, 2)
             total_time = time.time() - start_time
-            print('[{}, {:.8f}s] = [{}]'.format(name, total_time, ', '.join(sorted(similar))))
+            print('[{}, {:.8f}s] = [{}][{}]'.format(
+                name, total_time, len(similar), ', '.join(sorted(similar))))
 
         test_query_similar(tree_a, 'recursive bktree')
         test_query_similar(tree_b, 'iterative bktree')
--- tries_dist.py
+++ tries_dist.py
@@ -12,22 +12,20 @@ class TrieDist:
         self.trie[index][0] = True
 
     def get_similar(self, word, tolerance):
-        similar = {}
-
-        def recursive_get_similar(p, index, tol, curr, trie):
+        st, similar = [(0, 0, 0, '')], {}
+        while st:
+            p, index, tol, curr = st.pop()
             if p == len(word):
-                if trie[index][0]:
+                if self.trie[index][0]:
                     similar[curr] = True
             if p < len(word):
-                if word[p] in trie[index][1]:
-                    recursive_get_similar(p + 1, trie[index][1][word[p]], tol, curr + word[p], trie)
-                if tol > 0:
-                    recursive_get_similar(p + 1, index, tol - 1, curr, trie)
-                    for c, ind in trie[index][1].items():
-                        recursive_get_similar(p + 1, ind, tol - 1, curr + c, trie)
-            if tol > 0:
-                for c, ind in trie[index][1].items():
-                    recursive_get_similar(p, ind, tol - 1, curr + c, trie)
-
-        recursive_get_similar(0, 0, tolerance, '', self.trie)
+                if word[p] in self.trie[index][1]:
+                    st.append((p + 1, self.trie[index][1][word[p]], tol, curr + word[p]))
+                if tol < tolerance:
+                    st.append((p + 1, index, tol + 1, curr))
+                    for c, ind in self.trie[index][1].items():
+                        st.append((p + 1, ind, tol + 1, curr + c))
+            if tol < tolerance:
+                for c, ind in self.trie[index][1].items():
+                    st.append((p, ind, tol + 1, curr + c))
         return similar.keys()
